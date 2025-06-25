573af145132d92ba25c79aed669f1a55bfee75a0
Svyatoslav Shareko <modek@mail.ru>
2025-06-25 14:28:57 +0300

    Исправил ошибки в моделях и изменил модель Houses

--- app/database/model.py
+++ app/database/model.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
-from sqlalchemy import String, ForeignKey
+from sqlalchemy import String, Integer, ForeignKey
 
 
 class Base(DeclarativeBase):
@@ -11,16 +11,20 @@ class Organizations(Base):
 
     names: Mapped[str] = mapped_column(String(50), nullable=False)
     phones: Mapped[str] = mapped_column(String(75), nullable=False)
-    hauses_id: Mapped[int] = mapped_column(ForeignKey('Houses.id'), nullable=False)
-    activities_id: Mapped[int] = mapped_column(ForeignKey('Activities.id'), nullable=False)
+    houses_id: Mapped[int] = mapped_column(ForeignKey('Houses.id'),
+                                           nullable=False)
+    activities_id: Mapped[int] = mapped_column(ForeignKey('Activities.id'),
+                                               nullable=False)
 
-    houses: Mapped['Houses'] = relationship('Houses', back_populates='organizations')
-    activities: Mapped['Activities'] = relationship('Activities', back_populates='organizations')
+    houses: Mapped['Houses'] = relationship('Houses',
+                                            back_populates='organizations')
+    activities: Mapped['Activities'] = relationship('Activities',
+                                                    back_populates='organizations')
 
     def __repr__(self):
         return (f'Organizations(id={self.id!r}, '
                 f'name={self.names!r}, '
-                f'hause={self.hauses!r}, '
+                f'house={self.houses_id!r}, '
                 f'activity={self.activities!r})')
 
 
@@ -28,24 +32,29 @@ class Houses(Base):
     __tablename__ = 'Houses'
 
     address: Mapped[str] = mapped_column(String(50), nullable=False)
-    coordinates: Mapped[str] = mapped_column(String(30), nullable=False)
+    longitude: Mapped[str] = mapped_column(Integer(), nullable=False)
+    latitude: Mapped[str] = mapped_column(Integer(), nullable=False)
 
     organizations: Mapped[list[Organizations]] = relationship(back_populates='houses')
 
     def __repr__(self):
         return (f'Houses(id={self.id!r}, '
                 f'address={self.address!r}, '
-                f'coordinates={self.coordinates!r}')
+                f'longitude={self.longitude!r}, '
+                f'latitude={self.latitude!r})')
 
 
 class Activities(Base):
     __tablename__ = 'Activities'
 
+    id: Mapped[int] = mapped_column(autoincrement=True, primary_key=True)
     names: Mapped[str] = mapped_column(String(50), nullable=False)
-    parent_id: Mapped[int] = mapped_column(ForeignKey('Activities.id'), nullable=False)
+    parent_id: Mapped[int] = mapped_column(ForeignKey('Activities.id'),
+                                           nullable=True)
 
     organizations: Mapped[list[Organizations]] = relationship(back_populates='activities')
-    parent: Mapped['Activities'] = relationship('Activities', remote_side=[id], backref='children')
+    parent: Mapped['Activities'] = relationship('Activities', remote_side=[id],
+                                                backref='children')
 
     def __repr__(self):
         return (f'Activities(id={self.id!r}, '
