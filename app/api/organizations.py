5d3d4f5c687521d285b79fdf52e76c0184dd9157
Svyatoslav Shareko <modek@mail.ru>
2025-06-26 13:24:01 +0300

    Добавил описание эндпоинтов

--- app/api/organizations.py
+++ app/api/organizations.py
@@ -10,25 +10,65 @@ router = APIRouter()
 
 @router.get('/organizations_using_house/{house_id}', tags=['Organizations'])
 async def get_organizations_using_house(house_id: int):
+    '''
+    Возвращает все организации находящиеся в данном здании
+
+    Args:
+        id: ID здания
+
+    Returns: 
+        Список всех организаций в этом здании'''
     return await org.get_organizations_using_house(house_id)
 
 
 @router.get('/organizations_using_activity/{activity_id}', tags=['Organizations'])
 async def get_organizations_using_activity(activity_id: int):
+    '''
+    Возвращает все организации занимающеся данной деятельностью
+
+    Args:
+        id: ID деятельности
+
+    Returns: 
+        Список всех организаций занимающеся данной деятельностью'''
     return await org.get_organizations_using_activity(activity_id)
 
 
 @router.get('/organizations_using_coordinate/', tags=['Organizations'])
 async def get_organizations_using_coordinate(
         cerch_params: Annotated[SearchCircle | SearchRectangle, Body()]):
+    '''
+    Возвращает все организации попавшие в область поиска круга/квадрата
+
+    Args:
+        cerch_params: SearchCircle или SearchRectangle
+
+    Returns: 
+        Список всех организаций в область поиска круга/квадрата'''
     return await org.get_organizations_using_coordinate(cerch_params)
 
 
 @router.get('/organizations_using_id/{id}', tags=['Organizations'])
 async def get_organizations_using_id(id: int):
+    '''
+    Возвращает организацию по указанному id 
+
+    Args:
+        id: ID орагнизации
+
+    Returns: 
+        Список организации'''
     return await org.get_organizations_using_id(id)
 
 
 @router.get('/organizations_using_name/{name}', tags=['Organizations'])
 async def get_organizations_using_name(name: str):
+    '''
+    Возвращает организацию по указанному названию
+
+    Args:
+        name: Название орагнизации
+
+    Returns: 
+        Список организации'''
     return await org.get_organizations_using_name(name)
