39b15323ecf9214b8c2fcf07ad7a921503d82cf5
Svyatoslav Shareko <modek@mail.ru>
2025-07-22 17:34:28 +0300

    добавил первый тест

--- tests/test_api_organizations.py
+++ tests/test_api_organizations.py
@@ -13,48 +13,52 @@ async def test_organizations_using_house():
 
     async with AsyncClient(transport=ASGITransport(app),
                            base_url='http://test') as ac:
-        ...
+        response = await ac.get('/organizations_using_house/',
+                                params={'house_id': '1'})
+
+        print('\n\nResponse:', response.json())
+        assert response.status_code == 200
 
 
-@pytest.mark.asyncio
-async def test_organizations_using_activity():
-    '''
-    Позже будет подробная doc
-    '''
+# @pytest.mark.asyncio
+# async def test_organizations_using_activity():
+#     '''
+#     Позже будет подробная doc
+#     '''
 
-    async with AsyncClient(transport=ASGITransport(app),
-                           base_url='http://test') as ac:
-        ...
-
-
-@pytest.mark.asyncio
-async def test_organizations_using_coordinate():
-    '''
-    Позже будет подробная doc
-    '''
-
-    async with AsyncClient(transport=ASGITransport(app),
-                           base_url='http://test') as ac:
-        ...
+#     async with AsyncClient(transport=ASGITransport(app),
+#                            base_url='http://test') as ac:
+#         ...
 
 
-@pytest.mark.asyncio
-async def test_organizations_using_id():
-    '''
-    Позже будет подробная doc
-    '''
+# @pytest.mark.asyncio
+# async def test_organizations_using_coordinate():
+#     '''
+#     Позже будет подробная doc
+#     '''
 
-    async with AsyncClient(transport=ASGITransport(app),
-                           base_url='http://test') as ac:
-        ...
+#     async with AsyncClient(transport=ASGITransport(app),
+#                            base_url='http://test') as ac:
+#         ...
 
 
-@pytest.mark.asyncio
-async def test_organizations_using_name():
-    '''
-    Позже будет подробная doc
-    '''
+# @pytest.mark.asyncio
+# async def test_organizations_using_id():
+#     '''
+#     Позже будет подробная doc
+#     '''
 
-    async with AsyncClient(transport=ASGITransport(app),
-                           base_url='http://test') as ac:
-        ...
+#     async with AsyncClient(transport=ASGITransport(app),
+#                            base_url='http://test') as ac:
+#         ...
+
+
+# @pytest.mark.asyncio
+# async def test_organizations_using_name():
+#     '''
+#     Позже будет подробная doc
+#     '''
+
+#     async with AsyncClient(transport=ASGITransport(app),
+#                            base_url='http://test') as ac:
+#         ...
